8b84a076234963cfbcf47ce8c62f2e40e1600f37
vantammega <vantammega@gmail.com>
2016-05-02 12:46:58 +0700

    add files

---
+++ src/hello/Hello.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Tam
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic
+            DatagramSocket server;
+            InetAddress ip =  InetAddress.getByName("0.0.0.0");
+            server = new DatagramSocket(4444,ip);
+          byte[] buf = new byte[1000];
+            DatagramPacket dgp = new DatagramPacket(buf, buf.length);
+            server.receive(dgp);
+            System.out.println(dgp.getData());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Hello.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SocketException ex) {
+            Logger.getLogger(Hello.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Hello.class.getName()).log(Level.SEVERE, null, ex);
+        }
+);
+    }
+    
+}
