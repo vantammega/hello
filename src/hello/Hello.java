971779c01280d4714f5e8d5e4618779b5cf1463a
vantammega <vantammega@gmail.com>
2016-09-23 16:01:04 +0700

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -37,7 +37,7 @@ public class Hello {
          {
             Socket connectionSocket = welcomeSocket.accept();
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            outToClient.writeBytes("Hello nobi 12345678n");
+            outToClient.writeBytes("Hello nobi 123456789 \n");
             outToClient.close();
             connectionSocket.close();
          }
